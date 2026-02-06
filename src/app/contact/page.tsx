a0ca17c4b1c8a5ce67b520a33b7ab0e2cb8fd0a8
huzaifa baloch <huzaifa.baloch.9t@gmail.com>
2026-02-05 19:08:30 -0800

    update contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <div className="p-20 text-center">
-      <h1 className="text-3xl font-bold">Contact Us</h1>
+      <h1 className="text-3xl font-bold">Contact </h1>
       <p className="mt-4">📞 0334-4286113/03124127543</p>
       <p>📍 Lahore , Pakistan</p>
     </div>
